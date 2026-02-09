3a4091dfa438771b57ef22f1e730ffa18629cb99
Nicolas Loesel <loesel.nico@gmail.com>
2026-02-09 22:57:39 +0100

    Migration vers Mode Phoenix

--- src/components/InventoryCard.tsx
+++ src/components/InventoryCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import type { InventoryItem } from '../types';
 import { CATEGORIES } from '../constants';
 
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 export const Logo = () => (
   <div className="flex items-center gap-2">
